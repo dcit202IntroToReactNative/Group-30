de1f73887eed8c3a604f79abf9df0f67eacbf364
Kenny-skv <102988868+Kenny-skv@users.noreply.github.com>
2023-07-24 03:35:48 -0700

    Changed useRef hook

--- NotesApp/src/components/Note.js
+++ NotesApp/src/components/Note.js
@@ -5,7 +5,7 @@ import React, { useRef } from 'react';
 
 function Note({title, note, onChangeTitle, onChangeNotes}) {
 
-    const ref_input2 = useRef();
+    const ref_second_input = useRef();
 
     return (
         <StyledView>
@@ -15,14 +15,14 @@ function Note({title, note, onChangeTitle, onChangeNotes}) {
             numberOfLines={3} 
             autoFocus={true}
             returnKeyType="next"
-            onSubmitEditing={() => ref_input2.current.focus()}/>
+            onSubmitEditing={() => ref_second_input.current.focus()}/>
 
             <StyledInput className='mt-1 px-5 font-medium text-lg text-left' 
             onChangeText={onChangeNotes} 
             value={note} 
             multiline={true}   
             placeholder='Type anything here......' 
-            ref={ref_input2}
+            ref={ref_second_input}
             
             />
         </StyledView>
