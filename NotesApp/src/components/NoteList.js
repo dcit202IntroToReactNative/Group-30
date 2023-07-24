e116972c81c6ee85eaaeb03df2a43fc878b66f0e
Emmanuel-tetteh <102983590+Emmanuel-tetteh@users.noreply.github.com>
2023-07-24 05:16:39 -0700

    remove wasteful imports and duplicates

--- NotesApp/src/components/NoteList.js
+++ NotesApp/src/components/NoteList.js
@@ -16,7 +16,7 @@ function NoteList({ item }) {
             <TouchableOpacity
 
                 style={{ backgroundColor: '#fff', alignItems: 'flex-start', paddingLeft: 11, width: '50%', paddingVertical: 31, margin: '1%', borderRadius: 10 }}>
-                <StyledText className='text-left text-md font-bold' numberOfLines={2}>{titler}</StyledText>
+             
                 
             </TouchableOpacity>
         </>
