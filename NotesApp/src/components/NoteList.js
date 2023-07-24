0a1a2ccba1c993b12f3175eba369f2a298fd0bf7
Emmanuel-tetteh <102983590+Emmanuel-tetteh@users.noreply.github.com>
2023-07-24 05:16:00 -0700

    Removed unecessary imports

--- NotesApp/src/components/NoteList.js
+++ NotesApp/src/components/NoteList.js
@@ -17,7 +17,7 @@ function NoteList({ item }) {
 
                 style={{ backgroundColor: '#fff', alignItems: 'flex-start', paddingLeft: 11, width: '50%', paddingVertical: 31, margin: '1%', borderRadius: 10 }}>
                 <StyledText className='text-left text-md font-bold' numberOfLines={2}>{titler}</StyledText>
-                <StyledText className='text-left text-sm font-light' numberOfLines={3}>{noter}</StyledText>
+                
             </TouchableOpacity>
         </>
 
